0f1a14c639de7d2aecf4f8465d6aa731ef2fc113
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-09-08 14:41:46 -0700

    rounds working

--- src/hooks/useGetSingleJeopardy.js
+++ src/hooks/useGetSingleJeopardy.js
@@ -1,35 +1,36 @@
 import { useState, useEffect } from 'react';
 import { fetchCategoryById } from '../services/jservice';
-import { getRandomCategoryId } from '../data/getRandomCategoryIds';
+import { getRandomCategoryId, makeGameByRound } from '../data/getRandomCategoryIds';
 import validateCategory from '../data/categoryValidator';
 
-const useSingleJeopardy = () => {
-  const [singleJeopardy, setSingleJeopardy] = useState([]);
+const useJeopardyRound = (round = 'single') => {
+  const [jeopardyRound, setJeopardyRound] = useState([]);
   const [categoryComplete, setCategoryComplete] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  let singleJeopardyArray = [];
+  let jeopardyRoundArray = [];
   
-  const getSingleJeopardy = () => {
+  const getJeopardyRound = () => {
     setLoading(true);
     const categoryId = getRandomCategoryId();
     fetchCategoryById(categoryId)
       .then(validateCategory)
-      .then(category => category && singleJeopardyArray.push(category))
-      .then(() => singleJeopardyArray.length === 6 ? setCategoryComplete(true) : getSingleJeopardy())
-      .then(setSingleJeopardy(singleJeopardyArray))
-      .finally(() => setLoading(false))
+      .then(category => category && makeGameByRound(round, category))
+      .then(category => category && jeopardyRoundArray.push(category))
+      .then(() => jeopardyRoundArray.length === 6 ? setCategoryComplete(true) : getJeopardyRound())
+      .then(() => setLoading(false))
+      .finally(() => setJeopardyRound(jeopardyRoundArray))
       .catch();
   };
 
-  useEffect(getSingleJeopardy, []);
+  useEffect(getJeopardyRound, [round]);
   // I actually want this to trigger when the larger game state changes...
   
-  return [loading, singleJeopardy, categoryComplete];
+  return [loading, jeopardyRound, categoryComplete];
 
 };
 
-export default useSingleJeopardy;
+export default useJeopardyRound;
 
 
 //make a while loop instead of the Promise.all
